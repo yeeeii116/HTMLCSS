925fba14cfdc38dc4e525472ee937d087fe6acd1
yeeeii116 <yeeeii116@gmail.com>
2024-10-24 19:48:06 +0900

    first commit

---
+++ smoothieking/js/main.js
@@ -0,0 +1,30 @@
+$(function(){
+    const showNav = gsap.from('header', { 
+        yPercent: -100,
+        paused: true,
+        duration: 0.2
+        }).progress(1);
+
+        ScrollTrigger.create({
+        start: "top top",
+        end: "max",
+        // markers: false,
+        onUpdate: (self) => {
+            self.direction === -1 ? showNav.play() : showNav.reverse()
+        }
+    });
+    const showBg = gsap.from('.h_wp', { 
+        yPercent: -100,
+        paused: true,
+        duration: 0.2
+        }).progress(1);
+
+        ScrollTrigger.create({
+        start: "top top",
+        end: "max",
+        // markers: true,
+        onUpdate: (self) => {
+            self.direction === -1 ? showBg.play() : showBg.reverse()
+        }
+    });
+});
